d59230b2dd55d822d26cfce3626439c9d4ff1b5b
Craig Robbins <kde.psych@gmail.com>
2017-02-11 15:03:57 +1000

    Change some default values

--- config.lua
+++ config.lua
@@ -23,7 +23,8 @@ end
 --generation settings
 setting("number", "ymin", -33000) --bottom realm limit
 setting("number", "ymax", -700) --top realm limit
-setting("number", "tcave", 0.5) --cave threshold
+--setting("number", "tcave", 0.5) --cave threshold
+setting("number", "tcave", 0.75) --cave threshold
 
 --should player spawn in caves?
 setting("bool", "cavespawn", false)
@@ -33,13 +34,23 @@ setting("bool", "falling_icicles", true) --enable/disable falling icicles
 setting("number", "fallcha", 0.33) --chance of icicles falling when dug
 
 --decoration chances
-setting("number", "stagcha", 0.002) --chance of stalagmites
+--setting("number", "stagcha", 0.002) --chance of stalagmites
+--setting("number", "stalcha", 0.003) --chance of stalactites
+setting("number", "stagcha", 0.003) --chance of stalagmites
 setting("number", "stalcha", 0.003) --chance of stalactites
-setting("number", "h_lag", 15) --max height for stalagmites
-setting("number", "h_lac", 20) --...stalactites
-setting("number", "crystal", 0.007) --chance of glow crystal formations
-setting("number", "h_cry", 9) --max height of glow crystals
-setting("number", "h_clac", 13) --max height of glow crystal stalactites
+
+setting("number", "h_lag", 8) --max height for stalagmites
+setting("number", "h_lac", 8) --...stalactites
+setting("number", "crystal", 0.0002) --chance of glow crystal formations
+setting("number", "h_cry", 8) --max height of glow crystals
+setting("number", "h_clac", 8) --max height of glow crystal stalactites
+
+--setting("number", "h_lag", 15) --max height for stalagmites
+--setting("number", "h_lac", 20) --...stalactites
+--setting("number", "crystal", 0.007) --chance of glow crystal formations
+--setting("number", "h_cry", 9) --max height of glow crystals
+--setting("number", "h_clac", 13) --max height of glow crystal stalactites
+
 setting("number", "gemcha", 0.03) --chance of small glow gems
 setting("number", "mushcha", 0.04) --chance of mushrooms
 setting("number", "myccha", 0.03) --chance of mycena mushrooms
@@ -54,8 +65,8 @@ setting("number", "fortcha", 0.0003) --chance of fortresses
 setting("number", "dm_top", -4000) --upper limit 
 setting("number", "dm_bot", -5000) --lower limit 
 --should fortresses and fountains spawn?
-setting("bool", "fortresses", true)
-setting("bool", "fountains", true)
+setting("bool", "fortresses", false)
+setting("bool", "fountains", false)
 --Deep cave settings
 setting("number", "deep_cave", -7000) -- upper limit
 
