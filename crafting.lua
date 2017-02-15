a9b57da18980e51fda531abe01db9c12a9c43632
ezhh <owlecho@live.com>
2017-02-15 03:01:33 +0000

    Remove some recipes

--- crafting.lua
+++ crafting.lua
@@ -1,32 +1,6 @@
 --CaveRealms crafting.lua
 
---CRAFT ITEMS--
-
---mycena powder
-minetest.register_craftitem("caverealms:mycena_powder", {
-	description = "Mycena Powder",
-	inventory_image = "caverealms_mycena_powder.png",
-})
-
 --CRAFT RECIPES--
-
---mycena powder
-minetest.register_craft({
-	output = "caverealms:mycena_powder",
-	type = "shapeless",
-	recipe = {"caverealms:mycena"}
-})
-
-
---glow mese block
-minetest.register_craft({
-	output = "caverealms:glow_mese",
-	recipe = {
-		{"default:mese_crystal_fragment","default:mese_crystal_fragment","default:mese_crystal_fragment"},
-		{"default:mese_crystal_fragment","caverealms:mycena_powder","default:mese_crystal_fragment"},
-		{"default:mese_crystal_fragment","default:mese_crystal_fragment","default:mese_crystal_fragment"}
-	}
-})
 
 --reverse craft for glow mese
 minetest.register_craft({
