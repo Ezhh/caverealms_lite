f565c38793e3cb3aed6ff255713266e2e7d51d08
ezhh <owlecho@live.com>
2017-03-06 20:35:16 +0000

    Remove old unused settings

--- config.lua
+++ config.lua
@@ -23,12 +23,9 @@ end
 --generation settings
 setting("number", "ymin", -33000) --bottom realm limit
 setting("number", "ymax", -1500) --top realm limit
---setting("number", "tcave", 0.5) --cave threshold
 setting("number", "tcave", 0.75) --cave threshold
 
 --decoration chances
---setting("number", "stagcha", 0.002) --chance of stalagmites
---setting("number", "stalcha", 0.003) --chance of stalactites
 setting("number", "stagcha", 0.003) --chance of stalagmites
 setting("number", "stalcha", 0.003) --chance of stalactites
 
@@ -38,16 +35,9 @@ setting("number", "crystal", 0.0002) --chance of glow crystal formations
 setting("number", "h_cry", 8) --max height of glow crystals
 setting("number", "h_clac", 8) --max height of glow crystal stalactites
 
---setting("number", "h_lag", 15) --max height for stalagmites
---setting("number", "h_lac", 20) --...stalactites
---setting("number", "crystal", 0.007) --chance of glow crystal formations
---setting("number", "h_cry", 9) --max height of glow crystals
---setting("number", "h_clac", 13) --max height of glow crystal stalactites
-
 setting("number", "gemcha", 0.03) --chance of small glow gems
 setting("number", "mushcha", 0.04) --chance of mushrooms
 setting("number", "myccha", 0.03) --chance of mycena mushrooms
---setting("number", "wormcha", 0.02) --chance of glow worms
 setting("number", "wormcha", 0.015) --chance of glow worms
 setting("number", "giantcha", 0.001) --chance of giant mushrooms
 setting("number", "icicha", 0.035) --chance of icicles
